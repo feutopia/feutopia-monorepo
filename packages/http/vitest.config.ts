88393ab7e38bbc1596c958dd221ccbc411d8ab6d
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-11-12 20:17:24 +0800

    feat: @feutopia/http 更新 vitest.config.ts, 移除不需要的 setupFiles

--- packages/http/vitest.config.ts
+++ packages/http/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		environment: "jsdom",
-		globals: true,
-		setupFiles: ["./vitest.setup.ts"],
-	},
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
