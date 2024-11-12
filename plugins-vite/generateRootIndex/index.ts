f1fe91156dd78655a1b1f7e4cb28815c61a370cf
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-11-12 20:10:34 +0800

    feat: @feutopia plugins generateRootIndex 更新执行时机为 closeBundle

--- plugins-vite/generateRootIndex/index.ts
+++ plugins-vite/generateRootIndex/index.ts
@@ -3,13 +3,13 @@ import path from "path";
 import fs from "fs";
 
 export function generateRootIndex(): Plugin {
-	return {
-		name: "vite-plugin-generate-root-index",
-		generateBundle() {
-			const templatePath = path.resolve(__dirname, "./index.template.js");
-			const content = fs.readFileSync(templatePath, "utf-8");
-			const filePath = path.resolve(process.cwd(), "dist", "index.js");
-			fs.writeFileSync(filePath, content);
-		},
-	};
+  return {
+    name: "vite-plugin-generate-root-index",
+    closeBundle() {
+      const templatePath = path.resolve(__dirname, "./index.template.js");
+      const content = fs.readFileSync(templatePath, "utf-8");
+      const filePath = path.resolve(process.cwd(), "dist", "index.js");
+      fs.writeFileSync(filePath, content);
+    },
+  };
 }
