8bc205f80df6cfeb45ac90d5845300c128124284
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-11-15 14:41:51 +0800

    test: @feutopia/http 导出所有 axios 来的类型; 并且增加测试用例

--- packages/http/src/core/types.ts
+++ packages/http/src/core/types.ts
@@ -1,57 +1,59 @@
-import {
-	AxiosError,
-	AxiosRequestConfig,
-	AxiosResponse,
-	CreateAxiosDefaults,
+import type {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  CreateAxiosDefaults,
 } from "axios";
+
+export type * from "axios";
 
 // 1. 基础配置类型
 type RequestOptions = {
-	showError?: boolean;
+  showError?: boolean;
 };
 
 export type HttpRequestConfig = AxiosRequestConfig & RequestOptions;
 
 // 2. 基础响应和错误类型
 export interface HttpResponse<T = any> extends AxiosResponse {
-	data: T;
+  data: T;
 }
 
 export type HttpError = AxiosError & {
-	config?: HttpRequestConfig;
+  config?: HttpRequestConfig;
 };
 
 // 3. 处理器类型
 export type RequestHandler<T = any> = (
-	request: HttpRequestConfig
+  request: HttpRequestConfig
 ) => Promise<T> | T;
 
 export type ResponseHandler<T = any> = (
-	response: HttpResponse
+  response: HttpResponse
 ) => Promise<T> | T;
 
 export type ErrorHandler = (error: HttpError) => Promise<void> | void;
 
 // 4. 通用工具类型
 export interface CancellablePromise<T> extends Promise<T> {
-	cancel: () => void;
+  cancel: () => void;
 }
 
 export type ResponseType<T, R> = CancellablePromise<
-	R extends ResponseHandler<T> ? T : HttpResponse<T>
+  R extends ResponseHandler<T> ? T : HttpResponse<T>
 >;
 
 // 5. 配置相关类型
 export interface HttpClientOptions {
-	config?: CreateAxiosDefaults & RequestOptions;
-	requestHandler?: RequestHandler;
-	responseHandler?: ResponseHandler;
-	errorHandler?: ErrorHandler;
+  config?: CreateAxiosDefaults & RequestOptions;
+  requestHandler?: RequestHandler;
+  responseHandler?: ResponseHandler;
+  errorHandler?: ErrorHandler;
 }
 
 // 6. 特殊用途类型
 export interface UploadRequestConfig extends HttpRequestConfig {
-	onProgress?: (percentage: number) => void;
-	onUploadComplete?: (response: any) => void;
-	onUploadError?: (error: Error) => void;
+  onProgress?: (percentage: number) => void;
+  onUploadComplete?: (response: any) => void;
+  onUploadError?: (error: Error) => void;
 }
