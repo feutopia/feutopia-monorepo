69ff614502cb92fed277969d713b4cb86e8ef267
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-11-15 16:24:56 +0800

    refactor: @feutopia/hooks
    useRequest-Fetch 使用 # 代替 private 字段

--- packages/vue-hooks/src/useRequest/core/Fetch.ts
+++ packages/vue-hooks/src/useRequest/core/Fetch.ts
@@ -11,9 +11,9 @@ import {
 import { isNotRefObject } from "../utils";
 
 export class Fetch<TData, TParams extends any[]> {
-  private params: ValueOrEmptyArray<TParams>;
-  private count = 0;
-  private serviceTask: ReturnType<
+  #params: ValueOrEmptyArray<TParams>;
+  #count = 0;
+  #serviceTask: ReturnType<
     typeof buildCancelableTask<TData, DeepUnwrapRef<TParams>>
   > | null = null;
   state: FetchState<TData> = {
@@ -23,57 +23,56 @@ export class Fetch<TData, TParams extends any[]> {
     cancelled: shallowRef(false),
   };
   constructor(
-    private service: Service<TData, DeepUnwrapRef<TParams>>,
-    private options: RequestCallbackOptions<TData, TParams>,
-    private emitter: EmitterInstance<TData>
+    private readonly service: Service<TData, DeepUnwrapRef<TParams>>,
+    private readonly options: RequestCallbackOptions<TData, TParams>,
+    private readonly emitter: EmitterInstance<TData>
   ) {
-    this.service = service;
     this.options = options;
     this.emitter = emitter;
-    this.params = this.options.params ?? [];
+    this.#params = this.options.params ?? [];
   }
-  private setState(state: Partial<FetchStateData<TData>>) {
+  #setState(state: Partial<FetchStateData<TData>>) {
     for (const [key, value] of Object.entries(state)) {
       const item = this.state[key as keyof typeof state];
       item.value = value;
     }
   }
-  private resolveObj(params: Record<string, MaybeRefOrGetter<any>>) {
+  #resolveObj(params: Record<string, MaybeRefOrGetter<any>>) {
     const obj: Partial<typeof params> = {};
     for (const [key, value] of Object.entries(params)) {
       obj[key] = isNotRefObject(value)
-        ? this.resolveObj(value)
+        ? this.#resolveObj(value)
         : toValue(value);
     }
     return obj;
   }
-  private resolveParams(params: ValueOrEmptyArray<TParams>) {
+  #resolveParams(params: ValueOrEmptyArray<TParams>) {
     return params.map((param) =>
-      isNotRefObject(param) ? this.resolveObj(param) : toValue(param)
+      isNotRefObject(param) ? this.#resolveObj(param) : toValue(param)
     ) as DeepUnwrapRef<TParams>;
   }
-  private cancelTask() {
-    if (this.serviceTask) {
-      this.serviceTask.cancel();
-      this.serviceTask = null;
+  #cancelTask() {
+    if (this.#serviceTask) {
+      this.#serviceTask.cancel();
+      this.#serviceTask = null;
     }
   }
-  private async send(...args: ValueOrEmptyArray<TParams>) {
-    const params = this.resolveParams(args);
-    const currentCount = ++this.count;
-    this.cancelTask();
+  async #send(...args: ValueOrEmptyArray<TParams>) {
+    const params = this.#resolveParams(args);
+    const currentCount = ++this.#count;
+    this.#cancelTask();
     this.options.onBefore?.();
     this.emitter.emit("before");
-    this.setState({
+    this.#setState({
       loading: true,
       cancelled: false,
     });
-    this.serviceTask = buildCancelableTask(this.service);
-    const { data, cancelled, error } = await this.serviceTask.run(...params);
-    if (currentCount !== this.count) return this.state; // 如果不是当前的请求，直接返回
+    this.#serviceTask = buildCancelableTask(this.service);
+    const { data, cancelled, error } = await this.#serviceTask.run(...params);
+    if (currentCount !== this.#count) return this.state; // 如果不是当前的请求，直接返回
     if (cancelled) {
       // 取消
-      this.setState({
+      this.#setState({
         loading: false,
         error: undefined,
         cancelled: true,
@@ -81,7 +80,7 @@ export class Fetch<TData, TParams extends any[]> {
       this.emitter.emit("cancelled");
     } else if (error) {
       // 失败
-      this.setState({
+      this.#setState({
         loading: false,
         error,
         cancelled: false,
@@ -90,7 +89,7 @@ export class Fetch<TData, TParams extends any[]> {
       this.emitter.emit("error", error);
     } else {
       // 成功
-      this.setState({
+      this.#setState({
         loading: false,
         data,
         error: undefined,
@@ -104,16 +103,16 @@ export class Fetch<TData, TParams extends any[]> {
     return this.state;
   }
   run(...args: ValueOrEmptyArray<TParams>) {
-    return this.send(...args);
+    return this.#send(...args);
   }
   cancel() {
     this.emitter.emit("cancel");
-    this.cancelTask();
+    this.#cancelTask();
   }
   refresh() {
-    return this.send(...this.params);
+    return this.#send(...this.#params);
   }
-  public unmount() {
-    this.cancelTask();
+  unmount() {
+    this.#cancelTask();
   }
 }
