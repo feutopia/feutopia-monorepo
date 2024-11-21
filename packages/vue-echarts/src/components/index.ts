d0989b51ef438a3360e60a77f9b4a0f20c2b9006
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-11-21 14:40:46 +0800

    chore: packages/vue-charts 目录移除, 代码合并到 vue-ui

--- packages/vue-echarts/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export { default as BaseChart } from "./BaseChart/index.vue";
-export { default as BarChart } from "./BarChart/index.vue";
-export { default as LineChart } from "./LineChart/index.vue";
-export { default as PieChart } from "./PieChart/index.vue";
