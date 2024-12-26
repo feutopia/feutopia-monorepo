fd89e0efaf420fc2b50c13863139b0a0bd70c4d1
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-12-26 14:16:19 +0800

    chore: useRefExpose 兼容 element-plus 的组件实例

--- packages/vue-hooks/src/useRefExpose/index.ts
+++ packages/vue-hooks/src/useRefExpose/index.ts
@@ -1,9 +1,13 @@
-import { Ref } from "vue";
-import { ComponentPublicInstance } from "vue"; // 引入 Vue 实例类型
+import {
+  type ComponentPublicInstance,
+  type ComponentInternalInstance,
+  type Ref,
+} from "vue";
 
-export function useRefExpose<T extends ComponentPublicInstance>(
-  ref: Ref<T | undefined | null>
-) {
+// { $: ComponentInternalInstance } 为了兼容 element-plus 的组件实例的类型
+export function useRefExpose<
+  T extends { $: ComponentInternalInstance } | ComponentPublicInstance,
+>(ref: Ref<T | undefined | null>) {
   return new Proxy<T>({} as T, {
     get(_target, prop) {
       return ref.value?.[prop as keyof T];
