fe8f55f48e6fc214d929fe6846d20ef0ad5dccef
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2024-12-30 13:58:14 +0800

    chore: custom-scrollbar 编译修改
    1: babel 插件传入 extensions, 支持 .ts 文件编译
    2: 使用 rollup-plugin-typescript2 替换 @rollup/plugin-typescript

--- packages/custom-scrollbar/rollup.config.js
+++ packages/custom-scrollbar/rollup.config.js
@@ -1,74 +1,76 @@
 import { defineConfig } from "rollup";
 import del from "rollup-plugin-delete";
 import sass from "rollup-plugin-sass";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { babel } from "@rollup/plugin-babel";
 import strip from "@rollup/plugin-strip";
 import dts from "rollup-plugin-dts";
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+
+const sassOptions = {
+  options: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
+};
+
+const input = "lib/main.ts";
 
 export default defineConfig([
   {
-    input: "lib/main.ts", // 入口文件
+    input, // 入口文件
     output: [
       {
-        file: `dist/esm/CustomScrollbar.js`,
+        file: `dist/esm/index.js`,
         format: "esm",
       },
       {
-        file: `dist/cjs/CustomScrollbar.js`,
+        file: `dist/cjs/index.js`,
         format: "cjs",
       },
       {
-        file: `dist/umd/CustomScrollbar.js`,
+        file: `dist/umd/index.js`,
         format: "umd",
         name: "CustomScrollbar",
       },
     ],
-    /*
-    nodeResolve 插件为了让导入组件可以简写成:
-      import { debounce } from "./utils"
-    而不需要写成:
-      import { debounce } from "./utils/index"
-  */
-    // https://github.com/vitejs/vite/blob/9290d85b5d2ad64991bd296157cb3bcb959c341d/packages/vite/rollup.config.ts
     plugins: [
+      // 清理 dist 文件夹
       del({ targets: "dist/*" }),
+      // 处理 SASS 样式
       sass({
-        options: {
-          silenceDeprecations: ["legacy-js-api"],
-        },
+        ...sassOptions,
         output: true,
       }),
+      // 去除开发调试时的代码 (如console.log)
       strip({
-        include: "lib/**/*.(ts|js)",
+        include: "**/*.(js|ts)",
       }),
+      // 解析模块依赖（支持 node_modules）
+      nodeResolve(),
+      // 编译 TypeScript 文件，生成 JavaScript 输出
       typescript(),
+      // 转换 CommonJS 模块为 ES6 模块
       commonjs({
         sourceMap: false,
         strictRequires: "auto",
+        extensions: [".js", ".ts"],
       }),
-      nodeResolve(),
+      // 	使用 Babel 编译文件
       babel({
         babelHelpers: "bundled",
+        extensions: [...DEFAULT_EXTENSIONS, ".ts", ".tsx"],
       }),
     ],
   },
-  // 生成 .d.ts 声明文件的配置
+  // 生成 index.d.ts 声明文件的配置
   {
-    input: "lib/main.ts",
+    input,
     output: {
-      file: "dist/types/index.d.ts", // 声明文件输出
-      format: "esm", // ESM 格式
+      file: "dist/types/index.d.ts",
+      format: "esm",
     },
-    plugins: [
-      sass({
-        options: {
-          silenceDeprecations: ["legacy-js-api"],
-        },
-      }),
-      dts(),
-    ], // 使用 rollup-plugin-dts 插件生成声明文件
+    plugins: [sass({ ...sassOptions }), dts()],
   },
 ]);
