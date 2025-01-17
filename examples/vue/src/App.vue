d5eec5449f67911a503cf3c46dc93fff5cabbc8b
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2025-01-17 16:04:45 +0800

    feat: 虚拟滚动组件 -- 例子代码提交

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import Tabs from "./vue-ui/Tabs/index.tsx";
+import InfiniteList from "./vue-ui/InfiniteList/index.vue";
 import Dialog from "./vue-ui/Dialog/index.vue";
+import Tabs from "./vue-ui/Tabs/index.tsx";
 import LineChart from "./vue-ui/Echarts/LineChart.vue";
 import BarChart from "./vue-ui/Echarts/BarChart.vue";
 import PieChart from "./vue-ui/Echarts/PieChart.vue";
@@ -8,6 +9,7 @@ import "./http/cancelRequest.ts";
 </script>
 
 <template>
+  <InfiniteList />
   <Dialog />
   <Tabs />
   <LineChart />
